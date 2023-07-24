17e51784c991ef2e221e9f73ed73b0c60c26059d
Danise Rosales <daniserosales7@gmail.com>
2023-07-24 17:30:27 +0100

    header is now working!

--- client/src/pages/HomePage/index.jsx
+++ client/src/pages/HomePage/index.jsx
@@ -5,9 +5,9 @@ import { SearchWidget, Header } from '../../components';
 
 export default function HomePage() {
   return (
-  <>
+  <div className="homepage">
     <Header></Header>
     <SearchWidget ></SearchWidget>
-  </>
+  </div>
 )}
 
