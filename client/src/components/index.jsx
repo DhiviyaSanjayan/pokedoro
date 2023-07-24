a9a3d141769b44330932b42c977eeb741cbcb3eb
Danise Rosales <daniserosales7@gmail.com>
2023-07-24 08:33:21 +0100

    merging from dev

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,3 +1,4 @@
 export { default as LoginForm } from "./LoginForm";
 export { default as RegisterForm } from "./RegisterForm";
 export { default as CollectionCard } from "./CollectionCard";
+export { default as Header } from "./Header"
