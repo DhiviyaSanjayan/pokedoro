d303e1d11451af866e3c7846e64557ba12ca357c
Abdul Salim <100780536+salim1704@users.noreply.github.com>
2023-07-25 16:08:59 +0100

    pokemon mvc added

--- server/database/setup.sql
+++ server/database/setup.sql
@@ -1,14 +1,11 @@
-DROP TABLE IF EXISTS token;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS token CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
   users_id INT GENERATED ALWAYS AS IDENTITY,
   username VARCHAR(50) UNIQUE NOT NULL,
-  first_name VARCHAR(50) NOT NULL,
-  last_name VARCHAR(50) NOT NULL,
-  email VARCHAR(100),
   pass_word VARCHAR(100),
-  time_studied INT,
+  time_studied INT DEFAULT 0,
   PRIMARY KEY (users_id)
 );
 
@@ -20,15 +17,17 @@ CREATE TABLE token (
     FOREIGN KEY (users_id) REFERENCES users("users_id")
 );
 
-INSERT INTO users (username, first_name, last_name, email, pass_word, time_studied)
+INSERT INTO users (username, pass_word, time_studied)
 VALUES
-     ('ronaldo7', 'Cristiano', 'Ronaldo', 'cristiano@example.com', 'secure_password', 150),
-  ('messi10', 'Lionel', 'Messi', 'lionel@example.com', 'strong_password', 130),
-  ('neymarjr', 'Neymar', 'da Silva Santos', 'neymar@example.com', 'safepassword', 110),
-  ('mbappe7', 'Kylian', 'Mbappe', 'kylian@example.com', 'bestpassword', 100),
-  ('ronaldinho10', 'Ronaldinho', 'de Assis Moreira', 'ronaldinho@example.com', 'mypassword', 90),
-  ('beckham23', 'David', 'Beckham', 'david@example.com', 'beckham123', 80),
-  ('zidane5', 'Zinedine', 'Zidane', 'zidane@example.com', 'zidane456', 70),
-  ('pele10', 'Edson Arantes', 'do Nascimento', 'pele@example.com', 'pele789', 60),
-  ('maradona10', 'Diego', 'Maradona', 'maradona@example.com', 'maradona987', 50),
-  ('eusebio13', 'Eusébio', 'da Silva Ferreira', 'eusebio@example.com', 'eusebio654', 40);
+  ('ronaldo7','secure_password', 150),
+  ('messi10','strong_password', 130),
+  ('neymarjr', 'safepassword', 110),
+  ('mbappe7', 'bestpassword', 100),
+  ('ronaldinho10', 'mypassword', 90),
+  ('beckham23','beckham123', 80),
+  ('zidane5','zidane456', 70),
+  ('pele10', 'pele789', 60),
+  ('maradona10', 'maradona987', 50),
+  ('eusebio13', 'eusebio654', 40);
+
+  
