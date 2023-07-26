f4e2a65d048a8473c43aa5cd43ee5ffe62df35d4
Abdul Salim <100780536+salim1704@users.noreply.github.com>
2023-07-26 14:27:53 +0100

    minor changes backend

--- server/database/__tests__/pokedoro.test.js
+++ server/database/__tests__/pokedoro.test.js
@@ -1,9 +1,10 @@
-const pokemon = require("../../models/Pokemon");
+const Pokemon = require("../../models/Pokemon");
 const Token = require('../../models/Token');
 const authenticator = require('../../middleware/authenticator')
-const User = require('../../models/User');
+const User = require('../../models/User')
 const db = require('../connect');
 
+const request = require("supertest");
 const app = require("../../app");
 const { Pool } = require('pg')
 
@@ -42,124 +43,57 @@ describe('Authenticator Middleware', () => {
       expect(res.json).not.toHaveBeenCalled();
     });
     
-//     it('should handle missing or invalid token', async () => {
-//     // Mock missing or invalid token
-//     const missingToken = null;
-//     const invalidToken = 'invalid-token';
-
-//     // Mock Token.getOneByToken to return null for missing or invalid token
-//     Token.getOneByToken = jest.fn().mockReturnValueOnce(null);
-
-//     // Create a mock request object with the necessary headers
-//     const reqMissing = {
-//       headers: {
-//         authorization: missingToken,
-//       },
-//     };
-
-//     const reqInvalid = {
-//       headers: {
-//         authorization: invalidToken,
-//       },
-//     };
-
-//     // Create a mock response object with necessary methods
-//     const res = {
-//       status: jest.fn().mockReturnThis(),
-//       json: jest.fn(),
-//     };
-
-//     // Create a mock next function
-//     const next = jest.fn();
-
-//     // Call the authenticator middleware with missing token
-//     await authenticator(reqMissing, res, next);
-
-//     // Assert the flow for missing token
-//     expect(Token.getOneByToken).toHaveBeenCalledWith(missingToken);
-//     expect(next).not.toHaveBeenCalled();
-//     expect(res.status).toHaveBeenCalledWith(403);
-//     expect(res.json).toHaveBeenCalledWith({ error: `User not authenticated.` });
-
-//     // Reset mocks
-//     Token.getOneByToken.mockReset();
-//     res.status.mockClear();
-//     res.json.mockClear();
-
-//     // Call the authenticator middleware with invalid token
-//     await authenticator(reqInvalid, res, next);
-
-//     // Assert the flow for invalid token
-//     expect(Token.getOneByToken).toHaveBeenCalledWith(invalidToken);
-//     expect(next).not.toHaveBeenCalled();
-//     expect(res.status).toHaveBeenCalledWith(403);
-//     expect(res.json).toHaveBeenCalledWith({ error: `User's token doesn't match server` });
-//   });
-
-  it('should handle missing token', async () => {
-    // Mock missing token
+    it('should handle missing or invalid token', async () => {
+    // Mock missing or invalid token
     const missingToken = null;
-  
-    // Mock Token.getOneByToken to return null for missing token
+    const invalidToken = 'invalid-token';
+
+    // Mock Token.getOneByToken to return null for missing or invalid token
     Token.getOneByToken = jest.fn().mockReturnValueOnce(null);
-  
+
     // Create a mock request object with the necessary headers
     const reqMissing = {
       headers: {
         authorization: missingToken,
       },
     };
-  
-    // Create a mock response object with necessary methods
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    };
-  
-    // Create a mock next function
-    const next = jest.fn();
-  
-    // Call the authenticator middleware with missing token
-    await authenticator(reqMissing, res, next);
-  
-    // Assert the flow for missing token
-    expect(Token.getOneByToken).toHaveBeenCalledWith(missingToken);
-    expect(next).not.toHaveBeenCalled();
-    expect(res.status).toHaveBeenCalledWith(403);
-    expect(res.json).toHaveBeenCalledWith({ error: `User not authenticated.` });
-  });
-  
-  it('should handle invalid token', async () => {
-    // Mock invalid token
-    const invalidToken = 'invalid-token';
-  
-    // Mock Token.getOneByToken to return null for invalid token
-    Token.getOneByToken = jest.fn().mockReturnValueOnce(null);
-  
-    // Create a mock request object with the necessary headers
+
     const reqInvalid = {
       headers: {
         authorization: invalidToken,
       },
     };
-  
+
     // Create a mock response object with necessary methods
     const res = {
       status: jest.fn().mockReturnThis(),
       json: jest.fn(),
     };
-  
+
     // Create a mock next function
     const next = jest.fn();
-  
+
+    // Call the authenticator middleware with missing token
+    await authenticator(reqMissing, res, next);
+
+    // Assert the flow for missing token
+    expect(Token.getOneByToken).toHaveBeenCalledWith(missingToken);
+    expect(next).not.toHaveBeenCalled();
+    expect(res.status).toHaveBeenCalledWith(403);
+    expect(res.json).toHaveBeenCalledWith({ error: `User's token doesn't match server` });
+
+    // Reset mocks
+    Token.getOneByToken.mockReset();
+    res.status.mockClear();
+    res.json.mockClear();
+
     // Call the authenticator middleware with invalid token
     await authenticator(reqInvalid, res, next);
-  
+
     // Assert the flow for invalid token
     expect(Token.getOneByToken).toHaveBeenCalledWith(invalidToken);
     expect(next).not.toHaveBeenCalled();
     expect(res.status).toHaveBeenCalledWith(403);
     expect(res.json).toHaveBeenCalledWith({ error: `User's token doesn't match server` });
   });
-  
 })
