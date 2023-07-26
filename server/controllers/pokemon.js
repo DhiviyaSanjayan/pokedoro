c86270c3cfd937cbdbd492b02bcac9d785bc27de
“harry_tb2112” <harry_tb@icloud.com>
2023-07-26 12:11:40 +0100

    got collection stuff working

--- server/controllers/pokemon.js
+++ server/controllers/pokemon.js
@@ -1,48 +1,49 @@
 const Pokemon = require("../models/Pokemon");
 
 async function index(req, res) {
-    try {
-        const user = await Pokemon.getAll();
-        res.status(200).json(user);
-    } catch(error) {
-        res.status(500).json({"error": error.message})
-    }
+  try {
+    const user = await Pokemon.getAll();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
-async function getById(req,res){
-    try{
-        const id = parseInt(req.params.id);
-        const pokemon = await Pokemon.getOneByPokemonId(id);
-        res.status(200).json(pokemon)
-    } catch (error) {
-        res.status(404).json({"error": error.message})
-    } 
+async function getById(req, res) {
+  try {
+    const id = parseInt(req.params.id);
+    const pokemon = await Pokemon.getOneByPokemonId(id);
+    res.status(200).json(pokemon);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
 }
 
-async function getByName(req,res){
-    try{
-        const name = req.params.name
-        const pokemon = await Pokemon.getOneByName(name);
-        res.status(200).json(pokemon)
-    } catch (error) {
-        res.status(404).json({"error": error.message})
-    } 
+async function getByName(req, res) {
+  try {
+    const name = req.params.name;
+    const pokemon = await Pokemon.getOneByName(name);
+    res.status(200).json(pokemon);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
 }
 
-async function update (req, res) {
-    try {
-        const data = req.body;
-        const Id = parseInt(req.params.id)
-        const pokemon = await Pokemon.getOneByPokemonId(Id);
-        const result = await pokemon.update(data);
-       
-        res.status(200).json(result);
-    } catch (error) {
-        res.status(404).json({"error": error.message})
-    }
+async function update(req, res) {
+  try {
+    const data = req.body;
+    const Id = parseInt(req.params.id);
+    const pokemon = await Pokemon.getOneByPokemonId(Id);
+    const result = await pokemon.update(data);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
 }
-
 
 module.exports = {
-    index, getById, getByName, update
-  }
+  index,
+  getById,
+  getByName,
+  update,
+};
